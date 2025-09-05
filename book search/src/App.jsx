cac88d376d5c5759e64f1a9a2d14184917422cc9
Amisha99 <ramtekeamisha926@gmail.com>
2025-09-05 19:52:42 +0530

    Update App.jsx

--- book search/src/App.jsx
+++ book search/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">📚 Book Search App</h1>
+      <h1 className="text-3xl font-bold text-center mb-6">📚 Book Finder App</h1>
 
       <Routes>
         <Route
